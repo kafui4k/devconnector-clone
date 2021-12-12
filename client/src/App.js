8f6c4d2b7e48975dd4e117c44cd7a66384ffd768
Kafui Alordo <kalordo7@gmail.com>
2021-12-12 23:44:03 +0000

    Redux, Alerts Setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,31 @@ import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layouts/Alert';
 import './App.css';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Landing />} />
-      </Routes>
-      <section className="container">
-          <Routes>
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+        <section className="container">
+          <Alert />
+            <Routes>
+              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={<Login />} />
+            </Routes>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
